56aa0c6853508055fe566026f4d288ccbdfae3e9
Vaishnavip2706 <130354406+Vaishnavip2706@users.noreply.github.com>
2025-07-14 11:10:44 +0530

    React app
    React app

---
+++ App.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import './App.css';
+//import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+/*import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   } from "react-router-dom";*/
+function App() {
+  //whether dark mode is enabled
+  const[mode, setMode] = useState('light');
+  //Set Alert for modes
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    //Set time for alert message
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
+
+    const removeBodyClasses = ()=>{
+      document.body.classList.remove('bg-light')
+      document.body.classList.remove('bg-dark')
+      document.body.classList.remove('bg-warning')
+      document.body.classList.remove('bg-danger')
+      document.body.classList.remove('bg-success')
+    }
+  
+  const toggleMode= (cls)=>{
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls);
+
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","success");
+      document.title = 'TextUtils-Dark Mode';
+      /*setInterval(() =>{
+        document.title = 'TextUtils is Amazing Mode'
+      },2000);
+      setInterval(() =>{
+        document.title = 'Install TextUtils Now'
+      },1500);*/
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
+       document.title = 'TextUtils-Light Mode';
+    }
+  }
+  return (
+  <>
+    <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}></Navbar>
+    <Alert alert={alert}></Alert>
+    <div className="container my-3">
+      
+    <TextForm showAlert={showAlert} heading="Enter the text" mode={mode}></TextForm>
+
+  </div>
+  </>
+  );
+}
+
+export default App;
